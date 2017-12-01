8be329e79881737289a8629bf8a4f34a365d905f
LeeQH <quanhui9402@163.com>
2017-12-01 16:44:25 +0800

    测试分支提交

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -13,16 +13,36 @@
 	
 -- dasi用户操作
 -- drop table `STU_INFO`
--- 学生信息表
+-- 学生信息表(更新时，根据classid删除记录，后重新添加)
 CREATE TABLE `STU_INFO`(
  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
- `class_id` VARCHAR(32) COMMENT '班级id',
- `class_name` VARCHAR(32) COMMENT '班级名称', 
- `stu_name` VARCHAR(60) COMMENT '学生姓名',
- `par_name` VARCHAR(60) COMMENT '家长姓名',
- `tel_num` VARCHAR(20) COMMENT '电话号码',
+ ·stu_id· VARCHAR(32) COMMENT '学生id'
+ `stu_name` VARCHAR(32) COMMENT '学生姓名',
+ `par_name` VARCHAR(32) COMMENT '家长姓名',
+ `tel_num` VARCHAR(32) COMMENT '电话号码',
  `start_date` TIMESTAMP COMMENT '开通时间',
  `end_date` TIMESTAMP COMMENT '结束时间', 
+ `class_id` VARCHAR(32) COMMENT '班级id',
+ `class_name` VARCHAR(32) COMMENT '班级名称', 
  `last_date` TIMESTAMP COMMENT '最后一次上线时间',
+ PRIMARY KEY (`id`)
+)
+
+-- drop table `TEACHER_INFO`
+-- 教师信息表
+CREATE TABLE `TEACHER_INFO`(
+ `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+ `login_name` VARCHAR(32) COMMENT '登录名',
+ `login_pwd` VARCHAR(32) COMMENT '登录密码', 
+ PRIMARY KEY (`id`)
+)
+
+-- drop table `CLASS_INFO`
+-- 班级信息表
+CREATE TABLE `CLASS_INFO`(
+ `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+ `class_id` VARCHAR(32) COMMENT '班级id',
+ `class_name` VARCHAR(32) COMMENT '班级名', 
+ `teacher_id` INT(11) COMMENT '教师的id主键',
  PRIMARY KEY (`id`)
 )
