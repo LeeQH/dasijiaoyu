4dc4edb6d06e55b235907856fe3b9658a8318206
LeeQH <quanhui9402@163.com>
2017-12-01 18:57:22 +0800

    第二周内容

--- src/main/java/com/lqh/dasi/commen/URLConstant.java
+++ src/main/java/com/lqh/dasi/commen/URLConstant.java
@@ -2,27 +2,35 @@ package com.lqh.dasi.commen;
 
 /**
  * 地址常量
+ * 
  * @author LiQuanhui
  * @date 2017年11月24日 下午5:22:40
  */
 public final class URLConstant {
-	
-	/**登陆的URL:
-	 * http://ht.dasijiaoyu.com/dasi/userAction!userLogin.do*/
-	public static final String LOGIN_URL="http://ht.dasijiaoyu.com/dasi/userAction!userLogin.do";
-	
-	/**查询老师下有多少班级的URL:
-	 * http://ht.dasijiaoyu.com/dasi/allclAction!getAllclListByParam.do*/
-	public static final String QUERY_CLASS_URL="http://ht.dasijiaoyu.com/dasi/allclAction!getAllclListByParam.do";
-	
-	/**根据classsid查询该班学生信息的URLl:
-	 * http://ht.dasijiaoyu.com/dasi/studentAction!getStudentListGroupByName.do?classid=*/
-	public static final String QUERY_STUDENT_INFO_URL="http://ht.dasijiaoyu.com/dasi/studentAction!getStudentListGroupByName.do?classid=";
-	
-	/**根据classsid查询该班成绩排名的URL:
-	 * http://ht.dasijiaoyu.com/dasi/learninginfoAction!toBgHeroesList.do?classid=*/
-	public static final String QUERY_RANK_URL="http://ht.dasijiaoyu.com/dasi/learninginfoAction!toBgHeroesList.do?classid=";
 
-	/**class_path的路径*/
-	public static final String CLASS_PATH = Thread.currentThread().getContextClassLoader().getResource("/").getPath();  
+	/**
+	 * 登陆的URL: http://ht.dasijiaoyu.com/dasi/userAction!userLogin.do
+	 */
+	public static final String LOGIN_URL = "http://ht.dasijiaoyu.com/dasi/userAction!userLogin.do";
+
+	/**
+	 * 查询老师下有多少班级的URL:
+	 * http://ht.dasijiaoyu.com/dasi/allclAction!getAllclListByParam.do
+	 */
+	public static final String QUERY_CLASS_URL = "http://ht.dasijiaoyu.com/dasi/allclAction!getAllclListByParam.do";
+
+	/**
+	 * 根据classsid查询该班学生信息的URL:
+	 * http://ht.dasijiaoyu.com/dasi/studentAction!getStudentListGroupByName.do?classid=
+	 */
+	public static final String QUERY_STUDENT_INFO_URL = "http://ht.dasijiaoyu.com/dasi/studentAction!getStudentListGroupByName.do?classid=";
+
+	/**
+	 * 根据classsid查询该班成绩排名的URL:
+	 * http://ht.dasijiaoyu.com/dasi/learninginfoAction!toBgHeroesList.do?classid=
+	 */
+	public static final String QUERY_RANK_URL = "http://ht.dasijiaoyu.com/dasi/learninginfoAction!toBgHeroesList.do?classid=";
+
+	/** class_path的路径 */
+	public static final String CLASS_PATH = Thread.currentThread().getContextClassLoader().getResource("/").getPath();
 }
--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,14 +1,14 @@
 -- root用户操作
-	-- 创建数据库
-	CREATE DATABASE mydasi;
+-- 创建数据库
+CREATE DATABASE mydasi;
 
-	-- 创建用户
-	CREATE USER 'dasi'@'localhost' IDENTIFIED BY 'mykeyisthis'; 
+-- 创建用户
+CREATE USER 'dasi'@'localhost' IDENTIFIED BY 'mykeyisthis'; 
 
-	-- select USER FROM USER; 
+-- select USER FROM USER; 
 
-	-- 给dasi用户授予mydasi数据库的所有权
-	GRANT ALL PRIVILEGES ON mydasi.* TO 'dasi'@'localhost' IDENTIFIED BY 'mykeyisthis';
+-- 给dasi用户授予mydasi数据库的所有权
+GRANT ALL PRIVILEGES ON mydasi.* TO 'dasi'@'localhost' IDENTIFIED BY 'mykeyisthis';
 	
 	
 -- dasi用户操作
@@ -16,18 +16,18 @@
 -- 学生信息表(更新时，根据classid删除记录，后重新添加)
 CREATE TABLE `STU_INFO`(
  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
- ·stu_id· VARCHAR(32) COMMENT '学生id'
- `stu_name` VARCHAR(32) COMMENT '学生姓名',
- `par_name` VARCHAR(32) COMMENT '家长姓名',
- `tel_num` VARCHAR(32) COMMENT '电话号码',
- `start_date` TIMESTAMP COMMENT '开通时间',
- `end_date` TIMESTAMP COMMENT '结束时间', 
- `class_id` VARCHAR(32) COMMENT '班级id',
+ `stu_id` VARCHAR(32) DEFAULT NULL COMMENT '学生id',
+ `stu_name` VARCHAR(32) DEFAULT NULL COMMENT '学生姓名',
+ `par_name` VARCHAR(32) DEFAULT NULL COMMENT '家长姓名',
+ `tel_num` VARCHAR(32) DEFAULT NULL COMMENT '电话号码',
+ `start_date` DATE COMMENT '开通时间',
+ `end_date` DATE COMMENT '结束时间', 
+ `last_date` DATE COMMENT '最后一次上线时间',
+ `class_id` VARCHAR(32) COMMENT '班级id', 
  `class_name` VARCHAR(32) COMMENT '班级名称', 
- `last_date` TIMESTAMP COMMENT '最后一次上线时间',
  PRIMARY KEY (`id`)
-)
-
+);
+COMMIT;
 -- drop table `TEACHER_INFO`
 -- 教师信息表
 CREATE TABLE `TEACHER_INFO`(
@@ -35,7 +35,7 @@ CREATE TABLE `TEACHER_INFO`(
  `login_name` VARCHAR(32) COMMENT '登录名',
  `login_pwd` VARCHAR(32) COMMENT '登录密码', 
  PRIMARY KEY (`id`)
-)
+);
 
 -- drop table `CLASS_INFO`
 -- 班级信息表
@@ -45,5 +45,4 @@ CREATE TABLE `CLASS_INFO`(
  `class_name` VARCHAR(32) COMMENT '班级名', 
  `teacher_id` INT(11) COMMENT '教师的id主键',
  PRIMARY KEY (`id`)
-)
-
+);
